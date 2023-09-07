a6b554bf4b2f4ddc6cf944a90a947f5cb73d017c
RichaSingh00 <ririco613@gmail.com>
2023-09-07 17:48:17 +0530

    Add project files.

---
+++ Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApp
+{
+    enum Planets
+    {
+        earth=1,
+        mars=2,
+        neptune,
+        saturn=65,
+        venus,
+        mercury
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Three planets:");
+            Console.WriteLine("Total moon of Earth are: "+(int)Planets.earth);
+            Console.WriteLine("Total moons of Saturn are: "+(int)Planets.saturn);
+            Console.WriteLine(Planets.neptune);
+        }
+    }
+}
